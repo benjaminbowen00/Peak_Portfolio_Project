2fd019d64e23572e0b45ca8f560e3e6cc973adc6
benjaminbowen00 <benjibowen@hotmail.com>
2018-02-12 22:56:54 +0000

    getting comp name, ticker, number being printed - needs refactoring

--- front_end/src/models/companies_list.js
+++ front_end/src/models/companies_list.js
@@ -1,13 +1,11 @@
 const Request = require('../services/request.js');
 
-
 var CompaniesList = function() {
   this.companies = [];
   this.onLoad = null;
 }
 
 CompaniesList.prototype.getCompanies = function () {
-  // console.log("getCompanies being called");
   var request = new Request('http://localhost:5000/api/tickers');
   request.get(this.populateCompaniesArray.bind(this));
 };
--- front_end/src/views/modal_box.js
+++ front_end/src/views/modal_box.js
@@ -12,15 +12,17 @@ ModalBox.prototype.getCompaniesList = function () {
   completeCompaniesList.getCompanies();
 
   console.log("inside modal box", completeCompaniesList);
-
-  // console.log(completeCompaniesList.companies);
-  // return completeCompaniesList.companies
 };
+
+
+
+
 
 ModalBox.prototype.buildDatalistBox = function (data) {
 
-  console.log(this.companiesList);
   console.log("bulddataBox");
+  this.companiesList = data;
+  console.log(this.companiesList);
   // var modalContentDiv = document.createElement('div');
   var datalistCompanies = document.querySelector('#companies');
 
@@ -30,15 +32,38 @@ ModalBox.prototype.buildDatalistBox = function (data) {
     datalistCompanies.appendChild(option);
   })
 
+  var getTickerFromCompanyName = function(company){
+    var ticker = null;
+    data.forEach(function(companyFromList){
+      if (companyFromList.name === company){
+        ticker = companyFromList.ticker;
+      }
+    })
+
+    return ticker;
+  }
+
   var savePurchase = function(){
+    console.log("inside savePurchase", this);
     var selectedCompanyElement = document.querySelector('#selected-company')
     var numberOfSharesElement = document.querySelector('#number-of-shares')
-    var outputString = "You bought "+numberOfSharesElement.value+ " shares in "+selectedCompanyElement.value
+    var outputString = `You bought ${numberOfSharesElement.value} shares in ${selectedCompanyElement.value}. The ticker is ${getTickerFromCompanyName(selectedCompanyElement.value)}`
     console.log(outputString);
   }
+
+
+
 
   var savePurchaseButton = document.querySelector('#save-purchase-button');
   savePurchaseButton.addEventListener('click', savePurchase)
 
 }
+
+
+
+
+
+
+
+
 module.exports = ModalBox;
