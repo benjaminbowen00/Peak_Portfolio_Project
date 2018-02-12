0f96f450167508a5836a9273f280f2c2ee8e9a2a
DeepCFoxXx <learncode23@gmail.com>
2018-02-12 10:31:17 +0000

    Refactoring

--- front_end/src/views/modal_box.js
+++ front_end/src/views/modal_box.js
@@ -1,63 +1,61 @@
-
-
 function autocomplete(inp, arr) {
   console.log(inp, arr);
   var currentFocus;
 
   inp.addEventListener("input", function(e) {
-      var a, b, i, val = this.value;
+    var a, b, i, val = this.value;
 
-      closeAllLists();
-      if (!val) { return false;}
-      currentFocus = -1;
+    closeAllLists();
+    if (!val) { return false;}
+    currentFocus = -1;
 
-      a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
 
-      this.parentNode.appendChild(a);
-      for (i = 0; i < arr.length; i++) {
+    this.parentNode.appendChild(a);
+    for (i = 0; i < arr.length; i++) {
 
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
 
-          b = document.createElement("DIV");
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
+        b = document.createElement("DIV");
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
 
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
 
-          b.addEventListener("click", function(e) {
+        b.addEventListener("click", function(e) {
 
-              inp.value = this.getElementsByTagName("input")[0].value;
+          inp.value = this.getElementsByTagName("input")[0].value;
 
-              closeAllLists();
-          });
-          a.appendChild(b);
-        }
+          closeAllLists();
+        });
+        a.appendChild(b);
       }
+    }
   });
 
   inp.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
-      if (x) x = x.getElementsByTagName("div");
-      // if (e.keyCode == 40) {
-      //
-      //   currentFocus++;
-      //
-      //   addActive(x);
-      // } else if (e.keyCode == 38) { //up
-      //
-      //   currentFocus--;
-      //
-      //   addActive(x);
-      // } else if (e.keyCode == 13) {
-      //
-      //   e.preventDefault();
-      //   if (currentFocus > -1) {
-      //
-      //     if (x) x[currentFocus].click();
-      //   }
-      // }
+    var x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    // if (e.keyCode == 40) {
+    //
+    //   currentFocus++;
+    //
+    //   addActive(x);
+    // } else if (e.keyCode == 38) { //up
+    //
+    //   currentFocus--;
+    //
+    //   addActive(x);
+    // } else if (e.keyCode == 13) {
+    //
+    //   e.preventDefault();
+    //   if (currentFocus > -1) {
+    //
+    //     if (x) x[currentFocus].click();
+    //   }
+    // }
   });
   function addActive(x) {
     if (!x) return false;
@@ -85,8 +83,8 @@ function autocomplete(inp, arr) {
   }
 
   document.addEventListener("click", function (e) {
-      closeAllLists(e.target);
-      });
+    closeAllLists(e.target);
+  });
 }
 
 
