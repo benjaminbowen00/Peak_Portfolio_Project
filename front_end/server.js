f58c719189482bd403d5c1e123119ad89cbcc32b
Phil Mitchell <pm1873@gmail.com>
2018-02-14 12:45:52 +0000

    complete

--- front_end/server.js
+++ front_end/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const server = express();
+const path = require('path');
 
 server.use(express.static('build'));
 
 server.listen(3000, function(){
   console.log("Listening on port 3000");
 });
+
+server.get('/stock/:ticker', function(req, res) {
+  // console.log(req.params.ticker);
+  console.log(path.join(__dirname + '/build/stock.html'));
+  res.sendFile(path.join(__dirname + '/build/stock.html'));
+
+})
