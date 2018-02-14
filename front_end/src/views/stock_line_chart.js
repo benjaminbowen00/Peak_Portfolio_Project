742f08839a1fb746530bdd4add48ae11740b9fea
benjaminbowen00 <benjibowen@hotmail.com>
2018-02-14 13:21:43 +0000

    stock line chart done

---
+++ front_end/src/views/stock_line_chart.js
@@ -0,0 +1,56 @@
+const Highcharts = require('highcharts');
+
+var StockLineChart = function(dailyData) {
+
+var timeSeries = dailyData["Time Series (Daily)"];
+
+
+var dataArray = [];
+for(day in timeSeries){
+  dataArray.push([Date.parse(day), Number(timeSeries[day]["4. close"])]);
+};
+
+
+
+var container = document.querySelector('#stock-line-chart');
+
+var chart = new Highcharts.Chart({
+
+  chart: {
+    type: 'line',
+    renderTo: container
+  },
+
+  title: {
+    text: 'Share value'
+  },
+
+  yAxis: {
+    title: {
+      text: 'Value ($)'
+    }
+  },
+
+  xAxis: {
+    type: 'datetime'
+  },
+
+  series: [{
+    name: 'value',
+    data: dataArray;
+  }]
+
+});
+
+
+}
+
+module.exports = StockLineChart;
+
+
+
+
+
+
+
+}
